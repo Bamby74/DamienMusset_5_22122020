6f757ef6343e79e518ca00c5e62423ee392b3f07
Bamby74 <dam.musset@gmail.com>
2021-03-25 10:23:02 +0100

    correction erreur addProductToBasket

--- js/model/panier.js
+++ js/model/panier.js
@@ -137,6 +137,7 @@ const findGoodCam = (camSelected) => {
 
 //PRIX TOTAL POUR UN PRODUIT
 const calculCamPrice = (data,findData) => {
+    debugger
     let camQuantity = data.quantity;
     return findData.price/100 * camQuantity;
 }
